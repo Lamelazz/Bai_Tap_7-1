3f00eca4eb0ffddc04133e57d2e0e19370e424ec
Lamelazz <chanelthanmc@gmail.com>
2025-01-07 09:58:57 +0700

    Bai Tap 1 ngay 7/1

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.square}>
+        <Text style={styles.text}>Hello, world</Text>
+      </View>
     </View>
   );
 }
@@ -16,5 +17,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  square: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
   },
 });
